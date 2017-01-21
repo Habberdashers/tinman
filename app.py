9202acd8ab5e86dc0ea908eee45f721384cea281
shiva <shiva@velo-labs.com>
2017-01-21 15:54:32 -0800

    Added new training set

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-# encoding=utf8  
+# encoding=utf8 
+from flask import Flask, request, jsonify, json
 import sys
 import nltk
 import random
@@ -6,61 +7,76 @@ import codecs
 #from nltk.corpus import movie_reviews
 from nltk.classify.scikitlearn import SklearnClassifier
 import pickle
-
 from nltk.tokenize import word_tokenize
 
-
-bills_passed = open("passed.txt","r").read()
-bills_rejected = open("rejected.txt","r").read()
-
-
-documents = []
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 
-for r in bills_passed.split('\n'):
-    documents.append( (r, "pos") )
-
-
-for r in bills_rejected.split('\n'):
-    documents.append( (r, "neg") )
-
-all_words = []
-
-
-bills_passed_words = word_tokenize(bills_passed)
-bills_rejected_words = word_tokenize(bills_rejected)
-
-
-for w in bills_passed_words:
-    all_words.append(w.lower())
-
-for w in bills_rejected_words:
-    all_words.append(w.lower())
-
-all_words = nltk.FreqDist(all_words)
-
-word_features = list(all_words.keys())[:200]
-
-def find_features(document):
-    words = word_tokenize(document)
-    features = {}
-    for w in word_features:
-        features[w] = (w in words)
-    return features
-
-featuresets = [(find_features(rev), category) for (rev, category) in documents]
-
-random.shuffle(featuresets)
-
-# positive data example:      
-training_set = featuresets[:100]
-testing_set =  featuresets[100:]
+app = Flask(__name__, static_url_path='')
+app.debug = True
 
 
 
-classifier = nltk.NaiveBayesClassifier.train(training_set)
-print("Naive Bayes accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
-classifier.show_most_informative_features(10)
+
+
+@app.route("/hello", methods=['GET'])
+def say_hi():
+	return "Hello Tin Man"
 
 
 
+@app.route("/predict")
+def hello():
+	bills_passed = open("static/bills_passed.txt","r").read()
+	bills_rejected = open("static/bills_rejected.txt","r").read()
+
+	documents = []
+	for r in bills_passed.split('\n'):
+		documents.append( (r, "yes") )
+
+	for r in bills_rejected.split('\n'):
+		documents.append( (r, "no") )
+
+	all_words = []
+
+
+	bills_passed_words = word_tokenize(bills_passed)
+	bills_rejected_words = word_tokenize(bills_rejected)
+
+	for w in bills_passed_words:
+		all_words.append(w.lower())
+
+	for w in bills_rejected_words:
+		all_words.append(w.lower())
+
+	all_words = nltk.FreqDist(all_words)
+
+	word_features = list(all_words.keys())[:200]
+	
+	def find_features(document):
+		words = word_tokenize(document)
+		features = {}
+		for w in word_features:
+			features[w] = (w in words)
+		return features
+
+	featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+
+	random.shuffle(featuresets)
+
+	# positive data example:      
+	training_set = featuresets[:1000]
+	testing_set = featuresets[1000:]
+
+
+	classifier = nltk.NaiveBayesClassifier.train(training_set)
+	print("Naive Bayes accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+	#classifier.show_most_informative_features(1)
+
+	print (classifier.show_most_informative_features(5))
+	return jsonify({'status': 201, 'error': None, 'payload': {'response': classifier.show_most_informative_features(1), 'accuracy_percent': (nltk.classify.accuracy(classifier, testing_set))*100}})
+
+if __name__ == "__main__":
+	app.run()
